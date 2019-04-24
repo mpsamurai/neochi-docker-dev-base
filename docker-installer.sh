7d3146febb76e65c21eebfb673855ca1aa3983f3
Junya Kaneko <JunyaKaneko@users.noreply.github.com>
2019-04-24 18:53:31 +0900

    Updated to install older version of docker

--- docker-installer.sh
+++ docker-installer.sh
@@ -11,4 +11,7 @@ usermod -aG docker pi
 curl https://download.docker.com/linux/raspbian/gpg
 deb https://download.docker.com/linux/raspbian/ stretch stable >> /etc/apt/sources.list
 apt-get update
+apt-get purge docker-ce
+apt-get update
+apt-get install docker-ce=18.06.2~ce~3-0~raspbian
 systemctl start docker.service
