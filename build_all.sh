2e699b0802863357f95cae22c3c6420c38b29568
Junya Kaneko <junya@mpsamurai.org>
2019-04-28 10:14:45 +0900

    added build script.

---
+++ build_all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose-arm32v6-run-on-x64.yml build
+docker push mpsamurai/neochi-dev-base:20190424-arm32v6
+
+docker-compose -f docker-compose-x64.yml build
+docker push mpsamurai/neochi-dev-base:20190424-x64
