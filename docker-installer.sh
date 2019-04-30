d48a60662cde1c8c13a60a73580d8c456bb67e2d
Junya Kaneko <junya@mpsamurai.org>
2019-04-30 10:24:16 +0900

    constructed a nose test environment.

--- docker-installer.sh
+++ docker-installer.sh
@@ -5,6 +5,7 @@
 # You need to sudo to execute it.
 #
 
+apt-get update
 apt-get install apt-transport-https ca-certificates software-properties-common -y
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 usermod -aG docker pi
@@ -15,3 +16,6 @@ apt-get purge docker-ce
 apt-get update
 apt-get install docker-ce=18.06.2~ce~3-0~raspbian
 systemctl start docker.service
+apt-get -y install python3-dev python3-pip libffi-dev
+pip3 install --no-cache-dir docker-compose
+apt-get autoclean
