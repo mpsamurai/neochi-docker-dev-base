a79040ca0f65006b717bbecbdb26e37a1e7b163a
Junya Kaneko <junya@mpsamurai.org>
2019-05-01 00:42:24 +0900

    fixed path error.

--- build_all.sh
+++ build_all.sh
@@ -6,5 +6,5 @@ docker push mpsamurai/neochi-dev-base:20190424-arm32v6
 docker-compose -f docker-compose-x64.yml build
 docker push mpsamurai/neochi-dev-base:20190424-x64
 
-docker-compose -f docker-compose-x64.yml build
-docker push psamurai/neochi-dev-base:20190424-raspbian
+docker-compose -f docker-compose-raspbian-run-on-x64.yml build
+docker push mpsamurai/neochi-dev-base:20190424-raspbian
