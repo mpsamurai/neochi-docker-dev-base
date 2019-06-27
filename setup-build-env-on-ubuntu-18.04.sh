7376a51b87413590389de11b6144e95837381ddc
Junya Kaneko <junya@mpsamurai.org>
2019-06-27 14:55:33 +0900

    first commit

---
+++ setup-build-env-on-ubuntu-18.04.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-dev
+sudo sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker ubuntu
+sudo pip3 install docker-compose
+
+sudo apt-get -y install qemu-user-static
+cp /usr/bin/qemu-arm-static .
